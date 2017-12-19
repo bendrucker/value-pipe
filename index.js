6139b45a310172baf046bd4edb0c07d2c0ec7c8d
Andrew Joslin <andrewtjoslin@gmail.com>
2017-12-19 16:23:37 -0700

    Remove to-array dependency (#3)

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
 
-var toArray = require('to-array')
-
 module.exports = valuePipe
 
 function valuePipe (fns) {
   if (!fns) throw new TypeError('At least one function is required')
-  if (!Array.isArray(fns)) return valuePipe(toArray(arguments))
+  if (!Array.isArray(fns)) {
+    fns = Array.prototype.slice.call(arguments)
+  }
+
   return function valuePipeline (value) {
     for (var i = 0; i < fns.length; i++) {
       value = fns[i](value)
